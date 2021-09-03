171c428f81f4341bb60305611af1d800ad33f3bc
jawad <jawadhalbymakary@gmail.com>
2021-09-04 02:11:24 +0300

    styling the game

--- utils.js
+++ utils.js
@@ -13,9 +13,7 @@ class Beat {
 
 
 
-/**
- * Button class that keeps track of the button color based on a press
- */
+
 class Button {
     constructor(color, keyCode){
         this.color=color;
@@ -30,24 +28,18 @@ class Button {
             this.deselect()
         })
     }
-    /**
-     * Set the button color based on color specified
-     */
+  
     setButtonColorInHTML = () => {
         this.element.style.borderColor=this.color
     }
 
-    /**
-     * Select function to set the background color and boxShadow
-     */
+    
     select = () => {
     this.element.style.backgroundColor=this.color;
         this.element.style.boxShadow=`0px 0px 17px 0px ${this.color}`
     }
 
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
+   
     deselect = () => {
         this.element.style.backgroundColor='transparent'
         this.element.style.boxShadow='none'
